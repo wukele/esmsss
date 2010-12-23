4c9c46ad578f8006fe231fb6ce5d7902d179a68f
firefoxmmx <firefoxmmx@48c857e5-a7ed-b808-8f14-2ed0eb05d8fb>
2010-12-23 11:50:53 +0000

    添加 用户管理 主界面， 和添加页面细节。

--- trunk/WEMS/WebRoot/pages/admin/userAdmin/UserMain/UserAdd.js
+++ trunk/WEMS/WebRoot/pages/admin/userAdmin/UserMain/UserAdd.js
@@ -1,26 +1,45 @@
 /**
  * user manager add face
+ * 
  * @author ffmmx
  */
-(function(){
+(function() {
 	Ext.ns('com.wems.admin.userAdmin');
-	
-	com.wems.admin.userAdmin.UserAdd=function(){
+
+	com.wems.admin.userAdmin.UserAdd = function() {
 		var window;
 		var cm;
-		var submitprefix='user';
+		var submitprefix = 'user';
 		var store;
-		
+		var genderStore;
+
 		return {
-			getWindowCt:function(){
-				return window;		
+			clearWindowFieldValues : function() {
+				fields = window.findByType('textfield');
+				combos = window.findByType('combo');
+
+				Ext.each(fields, function(item, index, allItems) {
+					item.setValue('');
+				});
+				Ext.each(combos, function(item, index, allItems) {
+					item.setValue(1);
+				});
 			},
-			setDataStore:function(dataStore){
+			getWindowCt : function() {
+				return window;
+			},
+			setDataStore : function(dataStore) {
 				store = dataStore;
 			},
-			init:function(containerId){
-				window=new Ext.Window({
-					title:'添加用户',
+
+			init : function(containerId) {
+				genderStore = new Ext.data.SimpleStore({
+					fields : [ 'genderKey', 'genderName' ],
+					data : [ [ 1, '男' ], [ 0, '女' ] ],
+					id : 1
+				});
+				window = new Ext.Window({
+					title : '添加用户',
 					frame : true,
 					width : 300,
 					height : 130,
@@ -35,45 +54,105 @@
 						xtype : "textfield",
 						width : 180
 					},
-					items:[{fieldLabel:'用户号',name:'operNo',allowBlank:false,blankText:'用户号不能为空',maxLength:10,maxLengthText:'至多10位'},
-					       {fieldLabel:'用户名称',name:'operName',allowBlank:false,blankText:'用户名称不能为空',maxLength:60,maxLengthText:'至多60位'},
-					       {fieldLabel:'登录名称',name:'loginName',allowBlank:false,blankText:'登录名才不能为空',maxLength:60,maxLengthText:'至多60位'},
-					       {fieldLabel:'密码',name:'operPwd',allowBlank:false,blankText:'不能为空',maxLength:64,maxLengthText:'至多64位',inputType:'password'},
-					       {fieldLabel:'性别',name:'gender',xtype:'combo',triggerAction: 'all', mode: 'local',editable:false, data:[[1,'男'],[0,'女']]},
-					       {fieldLabel:'电话',name:'phone',maxLength:20,maxLengthText:'至多20位'},
-					       {fieldLabel:'位置',name:'positions',maxLength:32,maxLengthText:'至多32位'},
-					       {fieldLabel:'手机号码',name:'mobileNo',maxLength:20,maxLengthText:'至多20位'}
-					       ],
-					buttons:[{xtype:'button',name:'ok',text:'确认',handler:this.addUser},
-					         {xtype:'button',name:'cancel',text:'取消',handler:function(){window.hide();}}]
+					items : [ {
+						fieldLabel : '用户号',
+						name : 'operNo',
+						allowBlank : false,
+						blankText : '用户号不能为空',
+						maxLength : 10,
+						maxLengthText : '至多10位'
+					}, {
+						fieldLabel : '用户名称',
+						name : 'operName',
+						allowBlank : false,
+						blankText : '用户名称不能为空',
+						maxLength : 60,
+						maxLengthText : '至多60位'
+					}, {
+						fieldLabel : '登录名称',
+						name : 'loginName',
+						allowBlank : false,
+						blankText : '登录名才不能为空',
+						maxLength : 60,
+						maxLengthText : '至多60位'
+					}, {
+						fieldLabel : '密码',
+						name : 'operPwd',
+						allowBlank : false,
+						blankText : '不能为空',
+						maxLength : 64,
+						maxLengthText : '至多64位',
+						inputType : 'password'
+					}, {
+						fieldLabel : '性别',
+						id : 'gender',
+						name : 'gender',
+						xtype : 'combo',
+						typeAhead : true,
+						selectOnFocus : true,
+						triggerAction : 'all',
+						mode : 'local',
+						editable : false,
+						store : genderStore,
+						valueField : 'genderKey',
+						displayField : 'genderName'
+					}, {
+						fieldLabel : '电话',
+						name : 'phone',
+						maxLength : 20,
+						maxLengthText : '至多20位'
+					}, {
+						fieldLabel : '位置',
+						name : 'positions',
+						maxLength : 32,
+						maxLengthText : '至多32位'
+					}, {
+						fieldLabel : '手机号码',
+						name : 'mobileNo',
+						maxLength : 20,
+						maxLengthText : '至多20位'
+					} ],
+					buttons : [ {
+						xtype : 'button',
+						name : 'ok',
+						text : '确认',
+						handler : this.addUser
+					}, {
+						xtype : 'button',
+						name : 'cancel',
+						text : '取消',
+						handler : function() {
+							window.hide();
+						}
+					} ]
 				});
-				//初始化渲染容器
-				if(containerId)
+
+				// 初始化渲染容器
+				if (containerId)
 					window.render(containerId);
-				window.show();
 			},
-			addUser:function(){
+			addUser : function() {
 				fields = window.findByType('textfield');
 				combos = window.findByType('combo');
-				data={};
-				
-				Ext.each(fields,function(item,index,allItems){
-					var fieldName='';
-					if(submitprefix!=null)
-						fieldName=submitprefix+'.'+item.getName();
-					data[fieldName]=item.getValue();
+				data = {};
+
+				Ext.each(fields, function(item, index, allItems) {
+					var fieldName = '';
+					if (submitprefix != null)
+						fieldName = submitprefix + '.' + item.getName();
+					data[fieldName] = item.getValue();
 				});
-				Ext.each(combos,function(item,index,allItems){
-					var fieldName='';
-					if(submitprefix!=null)
-						fieldName=submitprefix+'.'+item.getName();
-					data[fieldName]=item.getValue();
+				Ext.each(combos, function(item, index, allItems) {
+					var fieldName = '';
+					if (submitprefix != null)
+						fieldName = submitprefix + '.' + item.getName();
+					data[fieldName] = item.getValue();
 				});
-				
+
 				Ext.Ajax.request({
-					url:''
+					url : ''
 				});
-				
+
 				window.hide();
 			}
 		};
--- trunk/WEMS/WebRoot/pages/admin/userAdmin/UserMain/UserMain.jsp
+++ trunk/WEMS/WebRoot/pages/admin/userAdmin/UserMain/UserMain.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=GBK"
     pageEncoding="GBK"%>
 <% String cp = request.getContextPath(); %>
+<script type="text/javascript" src="<%=cp %>/pages/admin/userAdmin/UserMain/UserAdd.js"></script>
 <script type="text/javascript" src="<%=cp %>/pages/admin/userAdmin/UserMain/UserMain.js"></script>
 <div id="UserMainPanel"></div>
