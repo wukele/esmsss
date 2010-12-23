fffd92370156e0840bbbca8bd36c12b574e7d118
firefoxmmx <firefoxmmx@48c857e5-a7ed-b808-8f14-2ed0eb05d8fb>
2010-12-23 11:50:53 +0000

    添加 用户管理 主界面， 和添加页面细节。

--- WEMS/WebRoot/pages/admin/userAdmin/UserMain/UserMain.js
+++ WEMS/WebRoot/pages/admin/userAdmin/UserMain/UserMain.js
@@ -1,72 +1,177 @@
 /**
  * 
  * user manager main ui
+ * 
  * @author ffmmx
  */
 
-(function(){
+(function() {
 	Ext.ns('com.wems.admin.userAdmin');
-	
-	com.wems.admin.userAdmin.UserMain=function(){
+
+	com.wems.admin.userAdmin.UserMain = function() {
 		var grid;
 		var store;
-		
-		var fieldModel=['operNo','operName','loginName','operPwd','gender','positions','phone','email','mobileNo'];
+
+		var fieldModel = [ 'operNo', 'operName', 'loginName', 'operPwd',
+				'gender', 'positions', 'phone', 'email', 'mobileNo' ];
 		var cm;
-		var sm=new Ext.grid.CheckboxSelectionModel();
+		var sm = new Ext.grid.CheckboxSelectionModel();
+		var UserAddCt;
+		var submitprefix = 'user';
+		
 		return {
-			init:function(contianerId){
+			me:this,
+			queryUser:function(){
+				if(!store.isDestroyed)
+					store.destroy();
+				var paras={};
+				var qryFields=grid.getTopToolbar().findByType('textfield');
+				Ext.each(qryFields,function(item,index,allItems){
+					var paraName='';
+					if(submitprefix)
+						paraName=submitprefix+'.'+item.getName();
+					paras[paraName]=item.getValue();
+				});
 				store=new Ext.data.JsonStore({
-					proxy:new Ext.data.MemoryProxy({lstUser:[{operNo:'1',operName:'测试',loginName:'登录名',operPwd:'ceshi',gender:'1'}]}),
-					fields:fieldModel,
-					idProperty:'operNo',
-					root:'lstUser'
+					proxy : new Ext.data.HttpProxy({
+						url : 'qryUser.action'
+					}),
+					baseParams:{jsonObject:paras},
+					fields : fieldModel,
+					idProperty : 'operNo',
+					root : 'lstUser'
+				});
+				store.reload();
+				
+			},
+			setUserAddCt : function(userAddCt) {
+				if (userAddCt)
+					UserAddCt = userAddCt;
+			},
+			init : function(contianerId) {
+				// for test ###
+				// store=new Ext.data.JsonStore({
+				// proxy:new
+				// Ext.data.MemoryProxy({lstUser:[{operNo:'1',operName:'测试',loginName:'登录名',operPwd:'ceshi',gender:'1'}]}),
+				// fields:fieldModel,
+				// idProperty:'operNo',
+				// root:'lstUser'
+				// });
+				store = new Ext.data.JsonStore({
+					proxy : new Ext.data.HttpProxy({
+						url : 'qryUser.action'
+					}),
+					fields : fieldModel,
+					idProperty : 'operNo',
+					root : 'lstUser'
 				});
 				store.load();
-				
-				cm=new Ext.grid.ColumnModel({columns:[ new Ext.grid.RowNumberer(),
-				                                           sm,
-				                                           {id:'operNo',header:'用户号',dataIndex:'operNo'},
-				                  		                 {header:'用户名称',dataIndex:'operName'},
-				                		                 {header:'登录名称',dataIndex:'loginName'},
-				                		                 {header:'密码',dataIndex:'operPwd'},
-				                		                 {header:'性别',dataIndex:'gender'},
-				                		                 {header:'位置',dataIndex:'positions'},
-				                		                 {header:'电话',dataIndex:'phone'},
-				                		                 {header:'电子邮件',dataIndex:'email'},
-				                		                 {header:'手机号码',dataIndex:'mobileNo'}],
-				                		        defaults:{
-				                		        	sortable:true
-				                		        }});
-//				store=new Ext.data.JsonStore({
-//					proxy:new Ext.data.HttpProxy({url:''}),
-//					fields:fieldModel,
-//					idProperty:'operNo',
-//					root:'lstUser'
-//				});
-				
-				
-				grid=new Ext.grid.GridPanel({
-					store:store,
-					layout:'fit',
-					title:'用户管理',
-					sm:sm,
-					cm:cm,
-					bbar:[{xtype:'button',name:'btnAddUser',text:'添加',handler:function(){
-						//添加用户方法
-					}},{xtype:'button',name:'btnRemoveUser',text:'删除',handler:function(){
-						//删除方法
-					}}]
+
+				cm = new Ext.grid.ColumnModel({
+					columns : [ new Ext.grid.RowNumberer(), sm, {
+						id : 'operNo',
+						header : '用户号',
+						dataIndex : 'operNo'
+					}, {
+						header : '用户名称',
+						dataIndex : 'operName'
+					}, {
+						header : '登录名称',
+						dataIndex : 'loginName'
+					}, {
+						header : '密码',
+						dataIndex : 'operPwd'
+					}, {
+						header : '性别',
+						dataIndex : 'gender'
+					}, {
+						header : '位置',
+						dataIndex : 'positions'
+					}, {
+						header : '电话',
+						dataIndex : 'phone'
+					}, {
+						header : '电子邮件',
+						dataIndex : 'email'
+					}, {
+						header : '手机号码',
+						dataIndex : 'mobileNo'
+					} ],
+					defaults : {
+						sortable : true
+					}
 				});
-				
-				if(contianerId)
+
+				grid = new Ext.grid.GridPanel({
+					store : store,
+					layout : 'fit',
+					title : '用户管理',
+					sm : sm,
+					cm : cm,
+					autoHeight:true,
+					height:500,
+					viewConfig:{autoFill:true},
+					tbar : [ {
+						xtype : 'panel',
+						layout : 'column',
+						frame:true,
+						items : [ {html:'用户号:',layout:'fit'},{
+								xtype : 'textfield',
+								width : 160,
+								fieldLabel : '用户号',
+								name : 'operNo',
+								maxLength : 10,
+								maxLengthText : '至多10位'
+							} , {html:'用户名称:',layout:'fit'},{
+								xtype : 'textfield',
+								width : 160,
+								fieldLabel : '用户名称',
+								name : 'operName',
+								maxLength : 60,
+								maxLengthText : '至多60位'
+							} , {
+							xtype : 'button',
+							name : 'query',
+							text : '查询',
+							handler :
+								// 查询用户方法
+								this.queryUser
+						} ]
+					} ],
+					bbar : [ {
+						xtype : 'button',
+						name : 'btnAddUser',
+						text : '添加',
+						handler : function() {
+							// 添加用户方法
+							// @add ffmmx
+							var addWindow = UserAddCt.getWindowCt();
+							UserAddCt.clearWindowFieldValues();
+							addWindow.show();
+						}
+					}, {
+						xtype : 'button',
+						name : 'btnRemoveUser',
+						text : '删除',
+						handler : function() {
+							// 删除方法
+							
+						}
+					} ]
+				});
+
+				if (contianerId)
 					grid.render(contianerId);
 			}
 		};
 	}();
 })();
 
+Ext.onReady(function() {
+	var UserAdd = com.wems.admin.userAdmin.UserAdd;
+	var UserMain = com.wems.admin.userAdmin.UserMain;
+	UserAdd.init();
+	UserMain.init('UserMainPanel');
+	UserMain.setUserAddCt(UserAdd);
 
-Ext.onReady(function(){
-	com.wems.admin.userAdmin.UserMain.init('UserMainPanel');
 });
